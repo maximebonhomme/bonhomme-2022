4e14a81470580d98d1ff82e3a1cd5660feb9dd69
Max Bonhomme <hello.bonhomme@gmail.com>
2022-07-27 14:48:06 +0200

    add 3x3 image and button

--- components/MintModal.js
+++ components/MintModal.js
@@ -11,6 +11,7 @@ import {
   Heading,
   Text,
   Link,
+  Image,
 } from '@chakra-ui/react';
 
 export const MintModal = ({ name, isOpen, onOpen, onClose }) => {
@@ -27,18 +28,38 @@ export const MintModal = ({ name, isOpen, onOpen, onClose }) => {
           borderRadius="3xl"
           textAlign="center"
         >
+          <Image
+            mx="auto"
+            mb={6}
+            width="96px"
+            src="/free3x3.svg"
+            alt="Free 3x3 NFT"
+          />
           <Text fontWeight="semibold" mb={2}>
             Thanks, {name}
           </Text>
           <Text color="blackAlpha.700">
             As you've come all this way and even connected your wallet, it's
-            only right to give you something in return. I've added your ENS to
-            friends list for my next project, Tamiko.
+            only right to give you something in return. You can mint one of my
+            3x3 generated NFTs for free.
           </Text>
-          <Button mt={4} mb={2}>
-            Mint
+          <Button
+            _hover={{ bg: 'blackAlpha.900' }}
+            bg="black"
+            color="white"
+            fontWeight="normal"
+            textTransform="uppercase"
+            fontSize="sm"
+            borderRadius="lg"
+            py={6}
+            mt={4}
+            mb={2}
+          >
+            Mint for free
           </Button>
-          <Link fontSize="12px">view contract on etherscan</Link>
+          <Link opacity={0.7} href="https://etherscan.io/" fontSize="12px">
+            view contract
+          </Link>
         </ModalContent>
       </Modal>
     </>
